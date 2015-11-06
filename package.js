f02aaf383bf605e25aead4e74038e910f756086d
Praney Behl <praneybehl@gmail.com>
2015-11-07 00:33:02 +1100

    package dependencies updated

--- package.js
+++ package.js
@@ -1,24 +1,23 @@
 Package.describe({
   name: 'praneybehl:react-notification',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  version: '3.0.2',
+  summary: 'Snackbar style notification component for React packaged for Meteor',
+  git: 'https://github.com/praneybehl/meteor-react-dropzone',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('ecmascript');
-  api.addFiles('react-notification.js');
+  api.use('react@0.1.13');
+  api.use('cosmos:browserify@0.8.3', 'client');
+  api.addFiles('package.browserify.js', 'client');
+  api.addFiles('package.browserify.options.json', 'client');
+  api.export('Notification');
 });
 
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('praneybehl:react-notification');
-  api.addFiles('react-notification-tests.js');
+
+Npm.depends({
+  "react-notification": "3.0.2",
+  "exposify": "0.5.0"
 });
